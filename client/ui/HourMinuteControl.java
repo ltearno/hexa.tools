252bf4ffb5ef3d667173eb9c1d5e9eea0462b712
Arnaud Tournier <ltearno@gmail.com>
2013-10-24 11:38:17 +0200

    added change callback for HourMinuteControl

--- client/ui/HourMinuteControl.java
+++ client/ui/HourMinuteControl.java
@@ -1,22 +1,33 @@
 package com.hexa.client.ui;
 
-import com.hexa.client.common.HexaTime;
-import com.hexa.client.common.text.NumberFormat;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.hexa.client.common.HexaTime;
+import com.hexa.client.common.text.NumberFormat;
 
 public class HourMinuteControl extends Composite
 {
+	public interface Callback
+	{
+		void onValueChanged( HexaTime newValue );
+	}
 
 	private static HourMinuteControlUiBinder uiBinder = GWT.create( HourMinuteControlUiBinder.class );
 
 	interface HourMinuteControlUiBinder extends UiBinder<Widget, HourMinuteControl>
 	{
 	}
+
+	Callback callback;
+	HandlerRegistration hourHandlerRegistration;
+	HandlerRegistration minuteHandlerRegistration;
 
 	@UiField
 	ListBox hour;
@@ -46,4 +57,31 @@ public class HourMinuteControl extends Composite
 	{
 		return new HexaTime( Integer.parseInt( hour.getValue( hour.getSelectedIndex() ) ), Integer.parseInt( minute.getValue( minute.getSelectedIndex() ) ), 0 );
 	}
+
+	public void setCallback( Callback callback )
+	{
+		this.callback = callback;
+
+		// unsubscribe previous registration, if any
+		if( hourHandlerRegistration != null )
+			hourHandlerRegistration.removeHandler();
+		// subscribe to changes in the 'hour' control
+		hourHandlerRegistration = hour.addChangeHandler( changeHandler );
+
+		// unsubscribe previous registration, if any
+		if( minuteHandlerRegistration != null )
+			minuteHandlerRegistration.removeHandler();
+		// subscribe to changes in the 'hour' control
+		minuteHandlerRegistration = minute.addChangeHandler( changeHandler );
+	}
+
+	private ChangeHandler changeHandler = new ChangeHandler()
+	{
+		@Override
+		public void onChange( ChangeEvent event )
+		{
+			if( callback != null )
+				callback.onValueChanged( getTime() );
+		}
+	};
 }
