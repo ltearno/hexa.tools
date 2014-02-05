3e159178e315f89ebb229c4d6174e7d4e2a9dade
Arnaud Tournier <ltearno@gmail.com>
2014-02-05 13:47:58 +0100

    remove warning

--- client/ui/dialog/DialogBoxForNormalWidget.java
+++ client/ui/dialog/DialogBoxForNormalWidget.java
@@ -4,7 +4,6 @@ import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.dom.client.Style.VerticalAlign;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.CloseEvent;
