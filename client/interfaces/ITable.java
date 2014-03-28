093ac91d8f200a83a5a13f5ca1ce35174b456b34
Arnaud Tournier <arnaud.tournier@rigpa.org>
2014-03-28 17:34:52 +0100

    remove itable domain

--- client/interfaces/ITable.java
+++ client/interfaces/ITable.java
@@ -3,6 +3,4 @@ package com.hexa.client.interfaces;
 public interface ITable
 {
 	String getName();
-
-	ITableDomain getTableDomain();
 }
--- client/interfaces/ITableDomain.java
+++
@@ -1,6 +0,0 @@
-package com.hexa.client.interfaces;
-
-public interface ITableDomain
-{
-	ITable[] getTables();
-}
