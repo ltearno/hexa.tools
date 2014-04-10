558a02c6bc5aeb28bc787440829625b87d4d28a5
Arnaud Tournier <arnaud.tournier@rigpa.org>
2014-04-10 17:04:14 +0200

    added CustomPanel to open some methods of the ComplexPanel

---
+++ client/ui/containers/CustomPanel.java
@@ -0,0 +1,18 @@
+package com.hexa.client.ui.containers;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.ComplexPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CustomPanel extends ComplexPanel
+{
+	public CustomPanel( Element element )
+	{
+		setElement( element );
+	}
+
+	public void addIn( Element element, Widget child )
+	{
+		add( child, element );
+	}
+}
