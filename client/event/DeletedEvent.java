e2ad6ea80f13dee34beb8a5c5c72a1e5d8f87225
Arnaud Tournier <arnaud.tournier@rigpa.org>
2014-04-10 16:35:07 +0200

    gwt DeletedEvent

---
+++ client/event/DeletedEvent.java
@@ -0,0 +1,40 @@
+package com.hexa.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.hexa.client.event.DeletedEvent.DeletedEventHandler;
+
+public class DeletedEvent<T> extends GwtEvent<DeletedEventHandler<T>>
+{
+	private T item;
+
+	public DeletedEvent( T item )
+	{
+		this.item = item;
+	}
+
+	public T getItem()
+	{
+		return item;
+	}
+
+	public interface DeletedEventHandler<T> extends EventHandler
+	{
+		void onDeleted( T item );
+	}
+
+	public static Type<DeletedEventHandler<?>> TYPE = new Type<>();
+
+	@SuppressWarnings( "unchecked" )
+	@Override
+	public com.google.gwt.event.shared.GwtEvent.Type<DeletedEventHandler<T>> getAssociatedType()
+	{
+		return (Type<DeletedEventHandler<T>>)(Type<?>)TYPE;
+	}
+
+	@Override
+	protected void dispatch( DeletedEventHandler<T> handler )
+	{
+		handler.onDeleted( item );
+	}
+}
