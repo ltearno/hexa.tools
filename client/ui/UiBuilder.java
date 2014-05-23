4fe10a8c9834d85800fd3a59d4f68634d88214fe
Arnaud Tournier <arnaud.tournier@rigpa.org>
2014-05-23 11:59:38 +0200

    UiBuilder : added vertical and horizontal panel construction methods

--- client/ui/UiBuilder.java
+++ client/ui/UiBuilder.java
@@ -1,7 +1,10 @@
 package com.hexa.client.ui;
 
 import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class UiBuilder
 {
@@ -11,5 +14,15 @@ public class UiBuilder
 			parent.add( widget );
 
 		return parent;
+	}
+
+	public static Widget horiz( IsWidget... children )
+	{
+		return addIn( new HorizontalPanel(), children );
+	}
+
+	public static Widget vert( IsWidget... children )
+	{
+		return addIn( new VerticalPanel(), children );
 	}
 }
