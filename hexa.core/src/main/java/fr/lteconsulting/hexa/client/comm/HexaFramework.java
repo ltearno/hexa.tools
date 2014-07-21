177e9f77878bb6fd77611e02abd0d9ff52b91d91
Arnaud Tournier <ltearno@gmail.com>
2014-07-21 09:23:17 +0200

    Added default images for HexaFramework

--- hexa.core/src/main/java/fr/lteconsulting/hexa/client/comm/HexaFramework.java
+++ hexa.core/src/main/java/fr/lteconsulting/hexa/client/comm/HexaFramework.java
@@ -1,6 +1,10 @@
 package fr.lteconsulting.hexa.client.comm;
 
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
+
+import fr.lteconsulting.hexa.client.comm.HexaFramework.ImageResources;
 
 public class HexaFramework
 {
@@ -27,5 +31,48 @@ public class HexaFramework
 		ImageResource dropdown();
 	}
 
-	public static ImageResources images = null;
+	public static ImageResources images = GWT.create( DefaultImages.class );
+}
+
+interface DefaultImages extends ClientBundle, ImageResources
+{
+	@Override
+	@Source( "blank.png" )
+	ImageResource blank();
+
+	@Override
+	@Source( "blank16.png" )
+	ImageResource blank16();
+
+	@Override
+	@Source( "16-em-cross.png" )
+	ImageResource delete();
+
+	@Override
+	@Source( "16-em-plus.png" )
+	ImageResource add();
+	
+	@Override
+	@Source( "16-em-plus.png" )
+	ImageResource addPlus();
+
+	@Override
+	@Source( "16-em-cross.png" )
+	ImageResource cancel();
+
+	@Override
+	@Source( "16-em-check.png" )
+	ImageResource ok();
+
+	@Override
+	@Source( "16-arrow-down.png" )
+	ImageResource treeMinus();
+
+	@Override
+	@Source( "16-arrow-right.png" )
+	ImageResource treePlus();
+	
+	@Override
+	@Source( "dropdown.png" )
+	ImageResource dropdown();
 }
