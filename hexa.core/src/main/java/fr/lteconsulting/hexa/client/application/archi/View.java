9e218f5aa025683abb17bff0375ed1e50be264ea
Arnaud Tournier <ltearno@gmail.com>
2016-04-07 15:23:06 +0200

    delete some useless interfaces

--- hexa.core/src/main/java/fr/lteconsulting/hexa/client/application/archi/View.java
+++ hexa.core/src/main/java/fr/lteconsulting/hexa/client/application/archi/View.java
@@ -5,14 +5,4 @@ import com.google.gwt.user.client.ui.Widget;
 public interface View
 {
 	Widget asWidget();
-
-	public interface SelectionHandler<T>
-	{
-		void onSelected( T selected );
-	}
-
-	public interface HasSelectionHandlers<T>
-	{
-		void addSelectionhandler( SelectionHandler<T> handler );
-	}
 }
