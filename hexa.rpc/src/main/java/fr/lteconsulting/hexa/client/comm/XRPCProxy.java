9dffcc4bdc1278ef5c269423ddd4bcfb0f839de4
Arnaud Tournier <ltearno@gmail.com>
2016-04-08 13:35:17 +0200

    comment

--- hexa.rpc/src/main/java/fr/lteconsulting/hexa/client/comm/XRPCProxy.java
+++ hexa.rpc/src/main/java/fr/lteconsulting/hexa/client/comm/XRPCProxy.java
@@ -3,6 +3,14 @@ package fr.lteconsulting.hexa.client.comm;
 import com.google.gwt.core.client.JavaScriptObject;
 import fr.lteconsulting.hexa.client.interfaces.IAsyncCallback;
 
+/**
+ * Callback used when some non frequent events happen on the RPC layer.
+ * 
+ * <p>Those events can be:<ul>
+ * <li>A server message which should be shown to the user,
+ * <li>An hang out request. This happens when the server requires more information that was originally provided for a rpc call to be made. The user has to answer a question like a comment or so on and and the rpc call can continue.
+ * </lu>
+ */
 public interface XRPCProxy
 {
 	void signalResultParseError( String parsedTxt, String trace );
