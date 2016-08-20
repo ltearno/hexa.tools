ca91c190145d034d29772b13253d234c13c1374c
Arnaud Tournier <ltearno@gmail.com>
2016-08-20 13:27:40 +0200

    angular2gwt - Subject rxjs

--- angular2-gwt/src/main/java/fr/lteconsulting/angular2gwt/client/interop/angular/rxjs/Subject.java
+++ angular2-gwt/src/main/java/fr/lteconsulting/angular2gwt/client/interop/angular/rxjs/Subject.java
@@ -5,4 +5,6 @@ import jsinterop.annotations.JsType;
 @JsType(isNative = true, namespace = "ng.rxjs", name = "Subject")
 public class Subject<T> extends Observable<T> {
 	public native void next(T item);
+
+	public native Observable<T> asObservable();
 }
